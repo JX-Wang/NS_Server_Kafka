a4f95312a7127364dad98064a1572cfc5e5e27a7
jx-wang <1411349759@qq.com>
2019-07-21 09:23:08 +0800

    first commit

---
+++ kafka_broker/kafka_producer.py
@@ -0,0 +1,39 @@
+# usr/bin/enc python
+# encoing:utf-8
+"""
+for learing kafka_broker
+===================
+Author @ wangjunxiong
+Date @ 2019/7/19
+"""
+from kafka import KafkaConsumer, KafkaProducer
+from time import sleep
+
+
+class kafka_producer:
+    def __init__(self):
+        self.bootstrap_sever = "10.245.146.115:9092"
+        self.topic = "ddivide6"
+
+    def push(self, values):
+        print "Start"
+        try:
+            producer = KafkaProducer(bootstrap_servers=[self.bootstrap_sever])  # topic->str brokers->list
+        except:
+            print "consumer read error"
+            return
+        # print 1
+        try:
+            producer.send(topic=self.topic, value=values)
+            producer.flush()  # Active sending
+
+            # sleep(5)
+        except Exception as e:
+            print "E Kafka send Error ->", str(e)
+            return
+
+
+if __name__ == '__main__':
+    kafka_producer().push(values="######")
+
+
